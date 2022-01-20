6aea50ea6e83a37a297e7c9d85e45b770054cf32
citixenken <kmuyesu@gmail.com>
2022-01-20 22:22:45 +0300

    more of ScrollView

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,14 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        GeometryReader { geo in
-            Image("books")
-                .resizable()
-                .scaledToFit()
-                .frame(width: geo.size.width * 0.8) //80% width of the screen
-                //.padding()
-                .frame(width: geo.size.width, height: geo.size.height) //centers view inside GR
+        ScrollView(.horizontal) {
+            LazyHStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomText("Item No. \($0)")
+                        .font(.title)
+                }
+            }
+            //.frame(maxWidth: .infinity)
         }
+    }
+}
+
+struct CustomText: View {
+    let text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(_ text: String) {
+        print("Creating a new CustomText")
+        self.text = text
     }
 }
 
