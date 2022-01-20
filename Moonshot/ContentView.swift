7b146464b89a2645561fa078f048d421b48232c8
citixenken <kmuyesu@gmail.com>
2022-01-20 14:47:10 +0300

    resizing images using GeometryReader

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,8 +9,14 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        GeometryReader { geo in
+            Image("books")
+                .resizable()
+                .scaledToFit()
+                .frame(width: geo.size.width * 0.8) //80% width of the screen
+                //.padding()
+                .frame(width: geo.size.width, height: geo.size.height) //centers view inside GR
+        }
     }
 }
 
