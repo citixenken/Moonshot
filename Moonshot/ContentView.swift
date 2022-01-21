cc6611e238c16a6cc5f47aa45029ab0b589784f8
citixenken <kmuyesu@gmail.com>
2022-01-21 12:51:22 +0300

    lazyvgrid and lazyhgrid

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -17,23 +17,15 @@ struct Address: Codable {
     let city: String
 }
 struct ContentView: View {
-    
+    let layout = [
+        GridItem(.adaptive(minimum: 60, maximum: 120))
+    ]
     var body: some View {
-        Button("Decode JSON") {
-            let input = """
-            {
-                "name": "Taylor Swift",
-                "address": {
-                    "street": "555, Taylor Swift Avenue",
-                    "city": "Nashville"
+        ScrollView {
+            LazyHGrid(rows: layout) {
+                ForEach(0..<1000) {
+                    Text("Item: \($0)")
                 }
-            }
-            """
-            
-            let data = Data(input.utf8)
-            let decoder = JSONDecoder()
-            if let user = try? decoder.decode(User.self, from: data) {
-                print(user.address)
             }
         }
     }
